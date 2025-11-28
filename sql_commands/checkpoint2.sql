bb17a4b6f3cbaf1d552fc3e10673f2809b4df3b6
Jaden Majid <majid.jaden@gmail.com>
2025-11-28 14:51:31 -0800

    updated c2 script

--- sql_commands/checkpoint2.sql
+++ sql_commands/checkpoint2.sql
@@ -4,6 +4,8 @@ When looking at this SQL file, there are 2 parts
   - FK ON X justifications are done as comments
 2. Insert statements
 */
+-- 0. drop all
+@@dropall.sql
 -- 1. Create Tables
 @@createtables.sql
 -- 2. Insert Statements
