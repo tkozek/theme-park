f85e81d46cade1ab5501aa2a0e5197991390c07e
jadenvm <jadenvm@students.cs.ubc.ca>
2025-11-28 18:22:24 -0800

    fixed do everything script

--- sql_commands/doeverything.sql
+++ sql_commands/doeverything.sql
@@ -3,7 +3,8 @@ BEGIN
     EXECUTE IMMEDIATE 'DROP TABLE ' || t.table_name || ' CASCADE CONSTRAINTS';
   END LOOP;
 END;
-;
+/
+
 CREATE TABLE Ride (
   RideName      VARCHAR2(255) PRIMARY KEY,
   Capacity      INTEGER,      
