adfe1c44a24f3160ab1f17e286d2f4a76efdc753
Jaden Majid <majid.jaden@gmail.com>
2025-11-28 18:00:35 -0800

    tiny changes

--- sql_commands/customers/select_guest_visits.sql
+++ sql_commands/customers/select_guest_visits.sql
@@ -1,4 +1,4 @@
 SELECT g.CustomerID, c.CustomerName, g.DateOfVisit
 FROM Guest g
 LEFT JOIN Customer c ON c.CustomerID = g.CustomerID
-ORDER BY g.DateOfVisit DESC, g.CustomerID
+ORDER BY  g.CustomerID
